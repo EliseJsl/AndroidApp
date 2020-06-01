72c7789deef0878d10005b2ab1cddc8b52cb0296
EliseJsl <jousselinelise@gmail.com>
2020-06-01 22:32:19 +0200

    picto traffic

--- RATP/app/src/main/java/fr/epf/ratp/TrafficAdapter.kt
+++ RATP/app/src/main/java/fr/epf/ratp/TrafficAdapter.kt
@@ -23,6 +23,28 @@ class TrafficAdapter(val traffics: List<String>) : RecyclerView.Adapter<TrafficA
     override fun onBindViewHolder(holder: TrafficViewHolder, position: Int) {
         val traffic = traffics[position]
         holder.trafficView.message_textview.text = traffic
+        holder.trafficView.picto_imageview.setImageResource(
+            when(position){
+                0 -> R.drawable.m1
+                1 -> R.drawable.m2
+                2 -> R.drawable.m3
+                3 -> R.drawable.m3bis
+                4 -> R.drawable.m4
+                5 -> R.drawable.m5
+                6 -> R.drawable.m6
+                7 -> R.drawable.m7
+                8 -> R.drawable.m7bis
+                9 -> R.drawable.m8
+                10 -> R.drawable.m9
+                11 -> R.drawable.m10
+                12 -> R.drawable.m11
+                13 -> R.drawable.m12
+                14 -> R.drawable.m13
+                15 -> R.drawable.m14
+                else -> R.drawable.m14
+            }
+
+        )
     }
 
 }
